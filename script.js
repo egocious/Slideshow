b58d4a1bdd6d5083b4f344f26d87471f21204675
Ego.Ugwu <egougwu11@yahoo.com>
2022-11-09 20:22:58 +0100

    First commit

--- script.js
+++ script.js
@@ -16,19 +16,3 @@ buttons.forEach(button=> {
         delete activeSlide.dataset.active
     })
 })
-/*
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-  }
-  dots[slideIndex-1].className += " active";
-}*/
